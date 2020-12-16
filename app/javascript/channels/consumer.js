5ed7722caa5baf9c61ee13a9deb8af8a9f06a7ea
Rahmanda Wahyu <rahmanda.1998@gmail.com>
2020-12-16 21:20:55 +0700

    add createCustomer params url heroku

--- app/javascript/channels/consumer.js
+++ app/javascript/channels/consumer.js
@@ -1,6 +1,11 @@
 // Action Cable provides the framework to deal with WebSockets in Rails.
 // You can generate new channels where WebSocket features live using the `bin/rails generate channel` command.
+//= require cable
+//= require_self
+//= require_tree .
 
 import { createConsumer } from "@rails/actioncable"
 
 export default createConsumer()
+
+createConsumer('http://chat-rails-simpel.herokuapp.com/cable')
